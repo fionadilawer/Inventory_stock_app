a17fcc5b3cb8da75d8d825530346a05d9f18f628
fionadilawer <fionadilawer@gmail.com>
2018-11-02 12:00:00 +0500

    added the pagination in the dashboard

--- client/src/Component/Products/productList/Productlist.jsx
+++ client/src/Component/Products/productList/Productlist.jsx
@@ -1,24 +1,27 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import "./productlist.scss";
 import { AiOutlineEye } from "react-icons/ai";
 import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Loader from '../../loading/Loader';
 import Search from '../../search/Search';
+import ReactPaginate from "react-paginate";
 import { useDispatch, useSelector } from "react-redux";
 import {
   FILTER_PRODUCTS,
   selectFilteredPoducts,
 } from "../../../Redux/product/FilterSlice";
-import { useEffect } from 'react';
 
 const Productlist = ({products, isLoading}) => {
 
+
+  //the filteredProducts is also "products", I only used it cause of filter func... in the redux
   const filteredProducts = useSelector(selectFilteredPoducts);
 
   const dispatch = useDispatch();
 
 
+  //search state
   const [search, setSearch] = useState("");
 
 
@@ -37,6 +40,26 @@ const Productlist = ({products, isLoading}) => {
   useEffect(()=>{
     dispatch(FILTER_PRODUCTS({ products, search }));
   },[products, search, dispatch]);
+
+
+  //Begin Pagination
+  const [currentItems, setCurrentItems] = useState([]);
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    const endOffset = itemOffset + itemsPerPage;
+
+    setCurrentItems(filteredProducts.slice(itemOffset, endOffset));
+    setPageCount(Math.ceil(filteredProducts.length / itemsPerPage));
+  }, [itemOffset, itemsPerPage, filteredProducts]);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % filteredProducts.length;
+    setItemOffset(newOffset);
+  };
+  //   End Pagination
 
 
   return (
@@ -72,7 +95,7 @@ const Productlist = ({products, isLoading}) => {
 
            {/* { mapping all the items from the product} */}
           <tbody>
-            {filteredProducts.map((product, index)=>{
+            {currentItems.map((product, index)=>{
               const { _id, name, category, price, quantity } = product;
               return (
                  <tr key={_id}>
@@ -105,8 +128,21 @@ const Productlist = ({products, isLoading}) => {
        )}
       </div>
 
+      <ReactPaginate
+          breakLabel="..."
+          nextLabel="Next"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="Prev"
+          renderOnZeroPageCount={null}
+          containerClassName="pagination"
+          pageLinkClassName="page-num"
+          previousLinkClassName="page-num"
+          nextLinkClassName="page-num"
+          activeLinkClassName="activePage"
+        />
       </div>
-      
     </div>
   )
 };
