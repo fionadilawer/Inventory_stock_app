a933987caf5e8e698311dee2543f96fad39eac12
fionadilawer <fionadilawer@gmail.com>
2018-11-02 12:00:00 +0500

    updated the verify user

--- backend/utils/verify.user.js
+++ backend/utils/verify.user.js
@@ -24,22 +24,3 @@ export const VerifyUserToken = asyncHandler(async (req, res, next)=>{
       }
 });
 
-
-
-
-
-
-
-// export const VerifyUserToken = asyncHandler( (req, res, next)=>{
-//     const token = req.cookies.access_token;
-
-//     //verifying if no token
-//     if (!token) return next(errorHandler(401, 'unauthorized'));
-
-//     //verifying the token
-//     Jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
-//         if (err) return next(errorHandler(403, 'Forbidden'));
-//         req.user = user;
-//         next();
-//     });
-// });
